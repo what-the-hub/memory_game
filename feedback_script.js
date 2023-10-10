2139bcba510d34bcc13bbf9dc3609595e3b67270
what-the-hub <iamthenight.yeah@gmail.com>
2023-10-10 10:42:54 +0300

    add feedback page with 3rd party validation lib

---
+++ feedback_script.js
@@ -0,0 +1,49 @@
+const validator = new JustValidate('#form')
+
+const badWords = ['bad', 'ugly', 'hate', 'not good', 'dislike', 'angry', 'bug', 'fix']
+
+validator
+  .addField('#name', [
+    {
+      rule: 'required'
+    },
+    {
+      rule: 'minLength',
+      value: 3
+    },
+    {
+      rule: 'maxLength',
+      value: 15
+    }
+  ])
+  .addField('#email', [
+    {
+      rule: 'required'
+    },
+    {
+      rule: 'email'
+    }
+  ])
+  .addField(
+    '#agree-checkbox',
+    [
+      {
+        rule: 'required'
+      }
+    ]
+  )
+  .addField('#feedback-message', [
+    {
+      validator: (value) => {
+        return value !== undefined && value.length > 3
+      },
+      errorMessage: 'Message should be more than 3 letters.'
+    },
+    {
+      validator: (value) => {
+        const pattern = new RegExp(badWords.join('|'), 'i')
+        return !pattern.test(value)
+      },
+      errorMessage: 'Message should contain only good feedback!'
+    }
+  ])
